a63a1e0e5242e0f527ccaccada784b80097557b2
riot020 <riottesting101@gmail.com>
2025-07-06 01:04:22 +0530

    Update send-email.js

--- cryptoreclaimpro/api/send-email.js
+++ cryptoreclaimpro/api/send-email.js
@@ -10,14 +10,14 @@ export default async function handler(req, res) {
     port: 465,
     secure: true,
     auth: {
-      user: process.env.EMAIL_USER, // Use environment variables in Vercel
-      pass: process.env.EMAIL_PASS,
+      user: "bebito0923@gmail.com", // Use environment variables in Vercel
+      pass: "loszhsmnmxuwnfaj",
     },
   });
 
   const mailOptions = {
     from: `"${name}" <${email}>`,
-    to: process.env.EMAIL_USER, // Or another inbox to receive messages
+    to: "bebito0923@gmail.com", // Or another inbox to receive messages
     subject: "New Form Submission",
     text: `From: ${name} <${email}>\n\n${message}`,
   };
@@ -30,3 +30,6 @@ export default async function handler(req, res) {
     res.status(500).send("Failed to send email.");
   }
 }
+
+GMAIL_USER=
+GMAIL_PASS=
