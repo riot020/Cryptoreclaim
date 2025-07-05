75f2946761384bb495a61065feb3c2e51ea5e1f1
riot020 <riottesting101@gmail.com>
2025-07-06 01:27:39 +0530

    Update send-email.js

--- cryptoreclaimpro/api/send-email.js
+++ cryptoreclaimpro/api/send-email.js
@@ -35,14 +35,14 @@ ${message}
     port: 465,
     secure: true,
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: "bebito0923@gmail.com",
+      pass: "loszhsmnmxuwnfaj",
     },
   });
 
   const mailOptions = {
     from: `"${fullName}" <${email}>`,
-    to: process.env.EMAIL_USER, // You receive the message
+    to: "bebito0923@gmail.com", // You receive the message
     subject: "New Scam Case Submission",
     text: formattedMessage,
   };
